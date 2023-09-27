cd0ff7141226aabaf135d3dd8814b3d7369599f5
mwssy <mokmail070@gmail.com>
2023-09-27 13:37:14 +0900

    css추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Editor from "./Editor.js";
 import Sidebar from "./Sidebar.js";
 
 export default function App({ $target }) {
-    new Sidebar({ $target })
+    const initialState = []
+    new Sidebar({ $target, initialState })
     new Editor({ $target })
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,40 @@
-export default function Sidebar({ $target }) {
-    const $sidebar = document.createElement("ul")
-    
-    $target.appendChild($sidebar)
+export default function Sidebar ({ $target, initialState }) {
+  const $sidebar = document.createElement("div")
 
+  const testData = [
+      {
+        "id": 1, // Document id
+        "title": "노션을 만들자", // Document title
+        "documents": [
+          {
+            "id": 2,
+            "title": "블라블라",
+            "documents": [
+              {
+                "id": 3,
+                "title": "함냐함냐",
+                "documents": []
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id": 4,
+        "title": "hello!",
+        "documents": []
+      }
+  ]
+
+  this.state = testData
+
+  $target.append($sidebar)
+
+  $sidebar.innerHTML = `
+      <ul>
+          ${this.state.map(item => `
+              ${item.id}<li>${item.title}<li>
+          `).join('')}
+      </ul>
+  `
 }
